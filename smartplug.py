524c75f640e000363fc71285b2f1494338bd96aa
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2023-10-28 18:37:50 -0400

    Cleaned some redundant annotations

--- smartplug.py
+++ smartplug.py
@@ -2,8 +2,7 @@ import zcs
 import asyncio
 from datetime import datetime
 from bleak import BleakClient
-from typing import Tuple
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
 
 class ResponseMismatchError(Exception):
     pass
@@ -14,13 +13,13 @@ class SmartplugBusyError(Exception):
 class MalformedCommandError(Exception):
     pass
 
-async def send_command(client, packet: bytearray):
+async def send_command(client: BleakClient, packet: bytearray) -> bytearray:
     await client.write_gatt_char(zcs.COMMAND_PIPE, data=packet,
                                         response=True)
     return await client.read_gatt_char(zcs.COMMAND_PIPE)
 
-async def send_commands(clients,
-                        packet: bytearray) -> AsyncGenerator[bytearray, None]:
+async def send_commands(clients: list[BleakClient],
+                        packet: bytearray) -> AsyncGenerator[bytearray]:
     response_queue = asyncio.Queue()
     tasks = set()
     for client in clients:
@@ -33,46 +32,46 @@ async def send_commands(clients,
     for i in range(len(clients)):
         yield await response_queue.get()
 
-async def send_commands_deaf(clients, packet: bytearray):
+async def send_commands_deaf(clients: list[BleakClient], packet: bytearray):
     await asyncio.gather(*[send_command(client, packet) for client in clients])
 
-def on(clients, brightness=0):
+def on(clients: list[BleakClient], brightness=0):
     return send_commands(clients, zcs.on(brightness))
 
-def off(clients):
+def off(clients: list[BleakClient]):
     return send_commands(clients, zcs.off())
 
-def set_mode(clients, is_appliance = True):
+def set_mode(clients: list[BleakClient], is_appliance = True):
     return send_commands(clients, zcs.set_mode(is_appliance))
 
-def sync_clock(clients):
+def sync_clock(clients: list[BleakClient]):
     return send_commands(clients, zcs.set_clock(datetime.today()))
 
-async def get_clock(clients) -> AsyncGenerator[datetime, None]:
+async def get_clock(clients: list[BleakClient]):
     async for response in send_commands(clients, zcs.get_clock()):
         yield zcs.parse_get_clock(response)
 
-async def read_power(clients) -> AsyncGenerator[int, None]:
+async def read_power(clients: list[BleakClient]):
     async for response in send_commands(clients, zcs.read_power()):
         yield zcs.parse_read_power(response)
 
-def add_schedule(clients, schedule: zcs.Schedule):
+def add_schedule(clients: list[BleakClient], schedule: zcs.Schedule):
     return send_commands(clients, zcs.add_schedule(schedule))
 
-async def get_schedule(client, i: int):
+async def get_schedule(client: BleakClient, i: int):
     return zcs.parse_get_schedule(await send_command(client,
                                                      zcs.get_schedule(i)))
 
-async def get_client_schedule_info(client):
+async def get_client_schedule_info(client: BleakClient):
     return zcs.parse_get_schedule_info(await send_command(client,
                                                    zcs.get_schedule_info()))
 
-async def get_client_schedules(client):
+async def get_client_schedules(client: BleakClient):
     schedule_info = await get_client_schedule_info(client)
     num_schedules = schedule_info[0]
     for i in range(1, num_schedules + 1):
         yield await get_schedule(client, i)
 
-async def remove_client_schedule(client, i: int):
+async def remove_client_schedule(client: BleakClient, i: int):
     schedule = await get_schedule(client, i)
     yield await send_command(client, zcs.remove_schedule(schedule))
