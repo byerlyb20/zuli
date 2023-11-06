482fdbe54f85c6903b012f71bea5934f4812868d
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2023-11-05 22:16:16 -0500

    Refactor translation layer to argparse

--- zuli.py
+++ zuli.py
@@ -2,6 +2,7 @@ import asyncio
 import zcs
 import smartplug
 import sys
+import argparse
 from datetime import time
 from bleak import BleakScanner
 from bleak import BleakClient
@@ -18,50 +19,60 @@ async def get_devices(clients):
     for client in clients:
         yield client.address
 
-TRANSLATION_LAYER = {
-    "on": (smartplug.on, lambda a : []),
-    "brightness": (smartplug.on, lambda a : [int(a[1])]),
-    "off": (smartplug.off, lambda a : []),
-    "mode": (smartplug.set_mode, lambda a : [a[1] != "dimmable"]),
-    "power": (smartplug.read_power, lambda a : []),
-    "time": (smartplug.get_clock, lambda a : []),
-    "synctime": (smartplug.sync_clock, lambda a : []),
-    "schedules": (get_schedules, lambda a : []),
-    "remove_schedule": (remove_schedule, lambda a : [int(a[1])]),
-    "add_schedule": (smartplug.add_schedule,
-                     lambda a : [zcs.Schedule(time=time.fromisoformat(a[1]),
-                                              action=zcs.Schedule.ACTION_OFF
-                                              if a[2] == "off"
-                                              else zcs.Schedule.ACTION_ON)]),
-    "write": (smartplug.send_commands, lambda a : [bytearray.fromhex(a[1])]),
-    "devices": (get_devices, lambda a : [])
-}
-
-async def do(args, devices):
-    # Use the translation layer to "translate" command line input to a zcs
-    # method call with appropriate arguments
-    translation = TRANSLATION_LAYER[args[0]]
-    command = translation[0]
-    command_args = translation[1](args)
-    
-    async for response in command(devices, *command_args):
-        if isinstance(response, bytearray):
-            print(response.hex())
-        else:
-            print(response)
-
 async def ainput(prompt: str) -> str:
     print(f"{prompt} ", end='', flush=True)
     return (await asyncio.to_thread(sys.stdin.readline)).rstrip('\n')
+    
+def configure_parser():
+    parser = argparse.ArgumentParser(prog="zuli", exit_on_error=False)
+    subparsers = parser.add_subparsers(title="command", required=True)
 
-async def command_prompt(devices):
-    raw_command = await ainput(f">>> ")
-    args = raw_command.split(" ")
-    if args[0] != "disconnect":
-        await do(args, devices)
-        return True
-    else:
-        return False
+    parser_on = subparsers.add_parser('on')
+    parser_on.add_argument('brightness', nargs='?', default=0, type=int)
+    parser_on.set_defaults(func=smartplug.on,
+                            params=lambda a : [a.brightness])
+
+    parser_off = subparsers.add_parser('off')
+    parser_off.set_defaults(func=smartplug.off)
+
+    parser_mode = subparsers.add_parser('mode')
+    parser_mode.add_argument('mode', choices=['dimmable', 'appliance'])
+    parser_mode.set_defaults(func=smartplug.set_mode,
+                                params=lambda a : [a.mode == 'appliance'])
+    
+    parser_power = subparsers.add_parser('power')
+    parser_power.set_defaults(func=smartplug.read_power)
+
+    parser_time = subparsers.add_parser('time')
+    parser_time.set_defaults(func=smartplug.get_clock)
+
+    parser_synctime = subparsers.add_parser('synctime')
+    parser_synctime.set_defaults(func=smartplug.sync_clock)
+
+    parser_schedule = subparsers.add_parser('schedules')
+    parser_schedule.set_defaults(func=get_schedules)
+
+    parser_schedule_remove = subparsers.add_parser('remove_schedule')
+    parser_schedule_remove.add_argument('schedule', type=int)
+    parser_schedule_remove.set_defaults(func=remove_schedule,
+                                        params=lambda a : [a.schedule])
+    
+    parser_schedule_add = subparsers.add_parser('add_schedule')
+    parser_schedule_add.add_argument('time', type=time.fromisoformat)
+    parser_schedule_add.add_argument('action', choices=['on', 'off'])
+    def schedule_params(a):
+        return [zcs.Schedule(time=a.time, action=zcs.Schedule.ACTION_OFF
+                                            if a.action == "off"
+                                            else zcs.Schedule.ACTION_ON)]
+    parser_schedule_add.set_defaults(func=smartplug.add_schedule,
+                                        params=schedule_params)
+    
+    parser_devices = subparsers.add_parser('devices')
+    parser_devices.set_defaults(func=get_devices)
+
+    subparsers.add_parser('quit')
+
+    return parser
 
 async def main():
     devices = {}
@@ -75,11 +86,24 @@ async def main():
     asyncio.create_task(discover())
     try:
         print("Ready. Devices will continue to connect in the background")
-        while await command_prompt(devices.values()):
-            # From the docs: "Setting the delay to 0 provides an optimized
-            # path to allow other tasks to run"
-            await asyncio.sleep(0)
-    except Exception as e:
+        parser = configure_parser()
+
+        while True:
+            command = await ainput(">>>")
+            if command.startswith("quit"):
+                break
+            try:
+                args = parser.parse_args(command.split())
+            except Exception as e:
+                print(e)
+                continue
+            params = args.params(args) if hasattr(args, 'params') else []
+            async for result in args.func(devices.values(), *params):
+                if isinstance(result, bytearray):
+                    print(result.hex())
+                else:
+                    print(result)
+    except argparse.ArgumentError as e:
         print(e)
     finally:
         print(f"Closing all connections")
