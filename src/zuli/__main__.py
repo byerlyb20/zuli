ac95346cbc5ed08226fd7b9f0beeab1d127d61b6
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2025-12-31 14:44:34 -0500

    Refactoring for new ZuliSmartplug class (should simplify HA integration)

--- src/zuli/__main__.py
+++ src/zuli/__main__.py
@@ -1,18 +1,18 @@
+from typing import Awaitable, Callable, TypeVar
 from . import protocol
-from . import smartplug
+from .smartplug import ZuliSmartplug
 import asyncio
 import sys
 import argparse
 from datetime import time
 from bleak import BleakScanner
-from bleak import BleakClient
 
 class InteractiveArgumentParser(argparse.ArgumentParser):
     def exit(self, status=0, message=None): # pyright: ignore[reportIncompatibleMethodOverride]
         if message:
             raise argparse.ArgumentError(argument=None, message=message)
 
-def filter_devices(devices: dict[str, BleakClient], addresses: list[str]):
+def filter_devices(devices: dict[str, ZuliSmartplug], addresses: list[str]):
     # An empty list of addresses returns all devices
     if len(addresses) == 0:
         return list(devices.values())
@@ -26,16 +26,26 @@ def filter_devices(devices: dict[str, BleakClient], addresses: list[str]):
             if k.startswith(addr):
                 filtered.append(v)
     return filtered
+
+T = TypeVar('T')
     
-def wrap_method(smartplug_func):
-    """Because methods in the smartplug module do not understand command line
-    arguments from argparse.Namespace objects, this method creates and returns
-    a wrapper method that reads command line arguments and then calls a
-    specified method in the smartplug module."""
-    async def do(args: argparse.Namespace, devices: dict[str, BleakClient]):
+def wrap_method(smartplug_func: Callable[[ZuliSmartplug], Callable[..., Awaitable[T]]]):
+    """Creates and returns a wrapper method that reads command line arguments
+    and then calls a specified method in the smartplug module."""
+    async def do(args: argparse.Namespace, devices: dict[str, ZuliSmartplug]):
         command_devices = filter_devices(devices, args.devices)
         method_params = args.params(args) if hasattr(args, 'params') else []
-        async for result in smartplug_func(command_devices, *method_params):
+
+        async def command_for_device(
+            device: ZuliSmartplug,
+            smartplug_func: Callable[..., Awaitable[T]],
+            args: list
+        ):
+            return (device, await smartplug_func(*args))
+
+        pending_commands = [command_for_device(device, smartplug_func(device), method_params) for device in command_devices]
+        for result_future in asyncio.as_completed(pending_commands):
+            result = await result_future
             device = result[0] if isinstance(result, tuple) else None
             result = result[1] if isinstance(result, tuple) else result
 
@@ -55,11 +65,11 @@ def wrap_method(smartplug_func):
                 else:
                     print(f"{device.address} : {result}")
             else:
-                print(result)
+                print(result)            
     return do
     
 async def list_devices(args: argparse.Namespace,
-                    devices: dict[str, BleakClient]):
+                    devices: dict[str, ZuliSmartplug]):
     for client in devices.values():
         print(client.address)
 
@@ -77,35 +87,35 @@ def configure_parser():
 
     parser_on = subparsers.add_parser('on', parents=[parent_parser])
     parser_on.add_argument('brightness', nargs='?', default=0, type=int)
-    parser_on.set_defaults(func=wrap_method(smartplug.on),
+    parser_on.set_defaults(func=wrap_method(lambda a: a.on),
                             params=lambda a : [a.brightness])
 
     parser_off = subparsers.add_parser('off', parents=[parent_parser])
-    parser_off.set_defaults(func=wrap_method(smartplug.off))
+    parser_off.set_defaults(func=wrap_method(lambda a: a.off))
 
     parser_mode = subparsers.add_parser('mode', parents=[parent_parser])
     parser_mode.add_argument('mode', choices=['dimmable', 'appliance'])
-    parser_mode.set_defaults(func=wrap_method(smartplug.set_mode),
+    parser_mode.set_defaults(func=wrap_method(lambda a: a.set_mode),
                                 params=lambda a : [a.mode == 'appliance'])
     
     parser_power = subparsers.add_parser('power', parents=[parent_parser])
-    parser_power.set_defaults(func=wrap_method(smartplug.read_power))
+    parser_power.set_defaults(func=wrap_method(lambda a: a.read_power))
 
     parser_time = subparsers.add_parser('time', parents=[parent_parser])
-    parser_time.set_defaults(func=wrap_method(smartplug.get_clock))
+    parser_time.set_defaults(func=wrap_method(lambda a: a.get_clock))
 
     parser_synctime = subparsers.add_parser('synctime',
                                             parents=[parent_parser])
-    parser_synctime.set_defaults(func=wrap_method(smartplug.sync_clock))
+    parser_synctime.set_defaults(func=wrap_method(lambda a: a.sync_clock))
 
     parser_schedule = subparsers.add_parser('schedules',
                                             parents=[parent_parser])
-    parser_schedule.set_defaults(func=wrap_method(smartplug.get_clients_schedules))
+    parser_schedule.set_defaults(func=wrap_method(lambda a: a.get_schedules))
 
     parser_schedule_remove = subparsers.add_parser('remove_schedule',
                                                 parents=[parent_parser])
     parser_schedule_remove.add_argument('schedule', type=int)
-    parser_schedule_remove.set_defaults(func=wrap_method(smartplug.remove_client_schedule),
+    parser_schedule_remove.set_defaults(func=wrap_method(lambda a: a.remove_schedule),
                                         params=lambda a : [a.schedule])
     
     parser_schedule_add = subparsers.add_parser('add_schedule',
@@ -116,7 +126,7 @@ def configure_parser():
         return [protocol.Schedule(time=a.time, action=protocol.ScheduleAction.OFF
                                             if a.action == "off"
                                             else protocol.ScheduleAction.ON)]
-    parser_schedule_add.set_defaults(func=wrap_method(smartplug.add_schedule),
+    parser_schedule_add.set_defaults(func=wrap_method(lambda a: a.add_schedule),
                                         params=schedule_params)
     
     parser_devices = subparsers.add_parser('devices')
@@ -127,29 +137,21 @@ def configure_parser():
     return parser
 
 async def main():
-    devices: dict[str, BleakClient] = {}
+    devices: dict[str, ZuliSmartplug] = {}
 
     async def discover():
         try:
             async with BleakScanner(service_uuids=[protocol.ZULI_SERVICE]) as scanner:
-                async with asyncio.TaskGroup() as tg:
-                    async for (device, advertisement_data) in scanner.advertisement_data():
-                        if device.address not in devices or not devices[device.address].is_connected:
-                            client = BleakClient(device)
-                            devices[device.address] = client
-
-                            async def try_connect(client: BleakClient):
-                                try:
-                                    await client.connect()
-                                except Exception:
-                                    pass
-                            tg.create_task(try_connect(client))
+                async for (device, advertisement_data) in scanner.advertisement_data():
+                    if device.address not in devices:
+                        zuli_device = ZuliSmartplug(device)
+                        devices[device.address] = zuli_device
         except asyncio.CancelledError:
             return
     discovery_task = asyncio.create_task(discover())
 
     try:
-        print("Ready. Devices will continue to connect in the background")
+        print("Ready. Discovery will continue in the background")
         parser = configure_parser()
 
         while True:
@@ -166,10 +168,10 @@ async def main():
     except Exception as e:
         print(e)
     finally:
-        print(f"Stopping discovery and closing all connections")
+        print(f"Stopping discovery and closing connections")
         discovery_task.cancel()
         await discovery_task
-        await asyncio.gather(*[client.disconnect()
-                                for client in devices.values()])
+        await asyncio.gather(*[device.disconnect()
+                                for device in devices.values()])
 
 asyncio.run(main())
--- src/zuli/smartplug.py
+++ src/zuli/smartplug.py
@@ -1,120 +1,116 @@
-from typing import Awaitable, Callable, Optional, TypeVar
+from typing import Awaitable, Callable, TypeVar
 from . import protocol
-import asyncio
 from datetime import datetime
 from bleak import BleakClient
-from collections.abc import AsyncGenerator
+from bleak.backends.device import BLEDevice
+from bleak_retry_connector import (
+    establish_connection,
+    BleakClientWithServiceCache
+)
 
-class ResponseMismatchError(Exception):
-    pass
-
-class SmartplugBusyError(Exception):
-    pass
-
-class MalformedCommandError(Exception):
+class UnexpectedResponseError(Exception):
     pass
 
 T = TypeVar('T')
-EncoderOrMessage = Callable[[BleakClient], Awaitable[bytearray | None]] | bytearray
-Decoder = Callable[[bytearray | None], T]
+EncoderOrMessage = Callable[[BleakClient], Awaitable[bytearray]] | bytearray
+Decoder = Callable[[bytearray], T]
 
-def decode_response_success(response: bytearray | None) -> bool:
-    if response == None:
-        return False
-    else:
-        return protocol.decode_response_status(response).is_success()
+def decode_response_success(response: bytearray) -> bool:
+    return protocol.decode_response_status(response).is_success()
 
-def decode_nullable_response_wrapper(decode_fun: Callable[[bytearray], T]) -> Callable[[bytearray | None], Optional[T]]:
-    def decode_nullable_response(response: bytearray | None) -> Optional[T]:
-        if response == None:
-            return None
+class ZuliSmartplug():
+
+    def __init__(self, device: BLEDevice):
+        self._device: BLEDevice = device
+        self._client: BleakClient | None = None
+
+    async def __get_connected_client(self) -> BleakClient:
+        if self._client and self._client.is_connected:
+            return self._client
         else:
-            return decode_fun(response)
-    return decode_nullable_response
+            return await establish_connection(
+                BleakClientWithServiceCache,
+                self._device,
+                self._device.name or self._device.address
+            )
+    
+    async def _send_command(
+        self,
+        encode_message: EncoderOrMessage,
+        decode_response: Decoder[T] = decode_response_success
+    ) -> T:
+        client = await self.__get_connected_client()
 
-async def _send_command(client: BleakClient, encode_message: EncoderOrMessage,
-                        decode_response: Decoder[T] = decode_response_success
-                        ) -> T:
-    message = await encode_message(client) if callable(encode_message) else encode_message
-    if message == None:
-        return decode_response(None)
-    await client.write_gatt_char(protocol.COMMAND_PIPE, data=message,
-                                 response=True)
-    raw_response = await client.read_gatt_char(protocol.COMMAND_PIPE)
-    return decode_response(raw_response)
-
-async def _send_commands(clients: list[BleakClient],
-                        encode_message: EncoderOrMessage,
-                        decode_response: Decoder[T] = decode_response_success
-                        ) -> AsyncGenerator[tuple[BleakClient, T]]:
-    async def send_command_wrap(client: BleakClient,
-                                encode_message: EncoderOrMessage,
-                                decode_response: Decoder[T]
-                                ) -> tuple[BleakClient, T]:
+        # Encode and send message
+        message = await encode_message(client) if callable(encode_message) else encode_message
+        await client.write_gatt_char(protocol.COMMAND_PIPE, data=message,
+                                    response=True)
+        
+        # Read and decode response
+        raw_response = await client.read_gatt_char(protocol.COMMAND_PIPE)
         try:
-            result = await _send_command(client, encode_message, decode_response)
-            return (client, result)
-        except Exception:
-            return (client, decode_response(None))
-    pending_commands = [send_command_wrap(client, encode_message, decode_response) for client in clients]
-    for result in asyncio.as_completed(pending_commands):
-        yield await result
+            return decode_response(raw_response)
+        except Exception as e:
+            raise UnexpectedResponseError() from e
+    
+    async def disconnect(self):
+        if self._client != None and self._client.is_connected:
+            await self._client.disconnect()
 
-def on(clients: list[BleakClient], brightness=0):
-    return _send_commands(clients, protocol.encode_on(brightness))
+    @property
+    def address(self):
+        return self._device.address
+    
+    async def on(self, brightness: int):
+        return await self._send_command(protocol.encode_on(brightness))
 
-def off(clients: list[BleakClient]):
-    return _send_commands(clients, protocol.encode_off())
+    async def off(self):
+        return await self._send_command(protocol.encode_off())
 
-def set_mode(clients: list[BleakClient], is_appliance = True):
-    return _send_commands(clients, protocol.encode_set_mode(is_appliance))
+    async def set_mode(self, is_appliance: bool):
+        return await self._send_command(protocol.encode_set_mode(is_appliance))
 
-def sync_clock(clients: list[BleakClient]):
-    return _send_commands(clients, protocol.encode_set_clock(datetime.today()))
+    async def sync_clock(self):
+        return await self._send_command(protocol.encode_set_clock(datetime.today()))
+    
+    async def get_clock(self):
+        return await self._send_command(
+            protocol.encode_get_clock(),
+            protocol.decode_get_clock
+        )
 
-def get_clock(clients: list[BleakClient]):
-    return _send_commands(clients,
-                          protocol.encode_get_clock(),
-                          decode_nullable_response_wrapper(protocol.decode_get_clock))
+    async def read_power(self):
+        return await self._send_command(
+            protocol.encode_read_power(),
+            protocol.decode_read_power
+        )
+    
+    async def add_schedule(self, schedule: protocol.Schedule):
+        return await self._send_command(protocol.encode_add_schedule(schedule))
 
-def read_power(clients: list[BleakClient]):
-    return _send_commands(clients,
-                          protocol.encode_read_power(),
-                          decode_nullable_response_wrapper(protocol.decode_read_power))
+    async def get_schedule(self, i: int):
+        return await self._send_command(
+            protocol.encode_get_schedule(i),
+            protocol.decode_get_schedule
+        )
 
-def add_schedule(clients: list[BleakClient], schedule: protocol.Schedule):
-    return _send_commands(clients, protocol.encode_add_schedule(schedule))
+    async def get_schedule_info(self):
+        return await self._send_command(
+            protocol.encode_get_schedule_info(),
+            protocol.decode_get_schedule_info
+        )
 
-async def get_schedule(client: BleakClient, i: int):
-    return await _send_command(client,
-                         protocol.encode_get_schedule(i),
-                         decode_nullable_response_wrapper(protocol.decode_get_schedule))
+    async def get_schedules_stream(self):
+        schedule_info = await self.get_schedule_info()
+        num_schedules = schedule_info['num_schedules']
 
-def get_client_schedule_info(clients: list[BleakClient]):
-    return _send_commands(clients,
-                          protocol.encode_get_schedule_info(),
-                          decode_nullable_response_wrapper(protocol.decode_get_schedule_info))
+        for i in range(1, num_schedules + 1):
+            yield await self.get_schedule(i)
+    
+    async def get_schedules(self):
+        schedules = [schedule async for schedule in self.get_schedules_stream()]
+        return sorted(schedules, key=lambda s: s.id)
 
-async def get_client_schedules(client: BleakClient):
-    schedule_info_result = await anext(get_client_schedule_info([client]))
-    schedule_info = schedule_info_result[1]
-    num_schedules = schedule_info['num_schedules'] if schedule_info != None else 0
-    for i in range(1, num_schedules + 1):
-        yield await get_schedule(client, i)
-
-async def get_clients_schedules(clients: list[BleakClient]):
-    async def get_schedules_wrapper(client: BleakClient):
-        schedules = [schedule async for schedule in get_client_schedules(client) if schedule != None]
-        return (client, schedules)
-    get_schedules_each_client = [get_schedules_wrapper(client) for client in clients]
-    for result in asyncio.as_completed(get_schedules_each_client):
-        yield await result
-
-async def remove_client_schedule(clients: list[BleakClient], i: int):
-    async def encode_remove_ith_schedule(client: BleakClient) -> bytearray | None:
-        schedule = await get_schedule(client, i)
-        if schedule == None:
-            return None
-        else:
-            return protocol.encode_remove_schedule(schedule)
-    yield await _send_command(clients[0], encode_remove_ith_schedule)
+    async def remove_schedule(self, i: int):
+        schedule = await self.get_schedule(i)
+        return await self._send_command(protocol.encode_remove_schedule(schedule))
